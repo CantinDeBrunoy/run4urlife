ec7a5e398f65b4e9ebfba7ebff38b479a5993c08
goodfoodtruck <mielatomique@protonmail.com>
2022-01-12 17:23:34 +0100

    #1 Init interface menu principal

--- front/src/components/interface-menu.component.js
+++ front/src/components/interface-menu.component.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const InterfaceMenuComponent = () => {
-    return <div></div>;
+    return (
+        <div className="menu">
+            <ul>
+                <li>Démarrer une partie</li>
+                <li>Tableau des scores</li>
+                <li>Paramètres</li>
+                <li>Aides</li>
+                <li>Crédits</li>
+            </ul>
+        </div>
+    );
 };
 
 export default InterfaceMenuComponent;
