a6236af787bd2548ed95e389ccf74a96a3efeeca
Pierre <hervelinp.code@gmail.com>
2022-01-13 11:59:40 +0100

    #14 create game.js

--- front/src/core/functions/helpers.js
+++ front/src/core/functions/helpers.js
@@ -3,3 +3,11 @@ export const getRandomInt = (min, max) => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const asyncTimeout = (timeout) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, timeout);
+    });
+};
