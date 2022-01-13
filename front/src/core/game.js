30a021ac070c3373769ff19001681e1c602cf0bf
Pierre <hervelinp.code@gmail.com>
2022-01-13 14:36:56 +0100

    #14 gameInit && Game constant

--- front/src/core/game.js
+++ front/src/core/game.js
@@ -1,10 +1,30 @@
 import { asyncTimeout } from './functions/helpers';
-import { deleteFirstLine } from './functions/map-generation';
+import { MapGeneration } from './functions/map-generation';
+import { Game } from './global';
 
 const mapDestructLoop = async () => {
-    await asyncTimeout(200);
-    deleteFirstLine();
+    await asyncTimeout(getTimeout());
+    MapGeneration.deleteFirstLine();
+    MapGeneration.createLine();
     requestAnimationFrame(mapDestructLoop);
 };
 
-export const gameInit = () => {};
+const getTimeout = () => {
+    let timeout = Game.difficulty.value * (Game.timer.value / 1000) ** 2 + 3000;
+    return timeout > 100 ? timeout : 100;
+};
+
+const startTimer = () => {
+    setInterval(() => {
+        Game.timer.nowDate = new Date();
+        Game.timer.value = Game.timer.nowDate.getTime() - Game.timer.startDate.getTime();
+    }, 100);
+};
+
+export const gameInit = () => {
+    MapGeneration.createGrid();
+    Game.timer.startDate = new Date();
+    Game.difficulty = Game.difficulties.Impossible;
+    startTimer();
+    mapDestructLoop();
+};
