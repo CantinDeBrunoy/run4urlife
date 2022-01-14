a814b44576f319674a32d41d8bff4585e3a9b14e
BuildTools <unconfigured@null.spigotmc.org>
2022-01-14 11:47:41 +0100

    update map generation tests

--- front/src/tests/map-generation.test.js
+++ front/src/tests/map-generation.test.js
@@ -1,62 +1,59 @@
-import { createGrid, createLine, deleteFirstLine } from '../core/functions/map-generation';
-import { ContentCaseType } from '../core/global';
+import { MapGenerationFunctions } from '../core/functions/map-generation';
+import { GlobalTypes, Game } from '../core/global';
 
 test('[createLine]: should add line to grid', () => {
-    const grid = [];
-    createLine(grid);
-    expect(grid).toStrictEqual([
+    MapGenerationFunctions.createLine();
+    expect(Game.grid).toStrictEqual([
         {
             id: 0,
             cases: [
                 {
-                    type: ContentCaseType.Empty,
+                    type: GlobalTypes.caseTypes.empty,
                 },
                 {
-                    type: ContentCaseType.Empty,
+                    type: GlobalTypes.caseTypes.empty,
                 },
                 {
-                    type: ContentCaseType.Empty,
+                    type: GlobalTypes.caseTypes.empty,
                 },
             ],
         },
     ]);
     for (let i = 0; i < 50; i++) {
-        createLine(grid);
+        MapGenerationFunctions.createLine();
     }
     let i = 0;
-    for (const line of grid) {
+    for (const line of Game.grid) {
         expect(line.id).toBe(i);
         if (line.id <= 3 || line.id % 2 === 0) {
             expect(line.cases).toStrictEqual([
                 {
-                    type: ContentCaseType.Empty,
+                    type: GlobalTypes.caseTypes.empty,
                 },
                 {
-                    type: ContentCaseType.Empty,
+                    type: GlobalTypes.caseTypes.empty,
                 },
                 {
-                    type: ContentCaseType.Empty,
+                    type: GlobalTypes.caseTypes.empty,
                 },
             ]);
         } else {
-            if (line.cases.includes(ContentCaseType.Obstacle)) {
-                expect(line.cases.includes(ContentCaseType.Empty)).toBe(true);
+            if (line.cases.includes(GlobalTypes.caseTypes.obstacle)) {
+                expect(line.cases.includes(GlobalTypes.caseTypes.empty)).toBe(true);
             }
         }
         i++;
     }
-    expect(grid.length).toBe(51);
+    expect(Game.grid.length).toBe(51);
 });
 
 test('[deleteFirstLine]: should delete first line of grid', () => {
-    const grid = [];
-    createLine(grid);
-    createLine(grid);
-    deleteFirstLine(grid);
-    expect(grid.length).toBe(1);
+    MapGenerationFunctions.createGrid();
+    MapGenerationFunctions.deleteFirstLine();
+    expect(Game.grid.length).toBe(4);
 });
 
 test('[createGrid]: should create grid with 5 lines', () => {
-    const grid = createGrid();
-    expect(grid.length).toBe(5);
+    MapGenerationFunctions.createGrid();
+    expect(Game.grid.length).toBe(5);
 });
