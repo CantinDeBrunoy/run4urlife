84c66799a85b5982ccb515522ed2e0ef2b19caae
goodfoodtruck <mielatomique@protonmail.com>
2022-01-23 16:25:23 +0100

    #11 game condition fix

--- front/src/components/menu/interface-menu.component.js
+++ front/src/components/menu/interface-menu.component.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Game } from '../../core/global';
 import { TabTypes } from '../../common/constant';
 import Toggle from '../animations/interface-animation-toggle.component';
 import InterfaceSettingsComponent from './interface-settings.component';
@@ -18,38 +19,23 @@ const InterfaceMenuComponent = (menu) => {
         }
         setOpen((open) => !open);
     };
-
-    if (menu.isMain) {
-        return (
-            <div>
-                <Toggle visible={open}>
-                    <nav className="menu">
-                        <ul>
-                            <li>Démarrer une partie</li>
-                            <li>Tableau des scores</li>
-                            <li onClick={() => toggle(TabTypes.Settings)}>Paramètres</li>
-                            <li>Aides</li>
-                            <li>Crédits</li>
-                        </ul>
-                    </nav>
-                </Toggle>
-                <Toggle visible={!open}>{component}</Toggle>
-            </div>
-        );
-    } else {
-        return (
-            <div>
+    return (
+        <div>
+            <Toggle visible={open}>
                 <nav className="menu">
                     <ul>
-                        <li>Reprendre la partie</li>
-                        <li>Paramètres</li>
+                        <li>{`${Game.state ? 'Reprendre la partie' : 'Démarrer une partie'}`}</li>
+                        <li>Tableau des scores</li>
+                        <li onClick={() => toggle(TabTypes.Settings)}>Paramètres</li>
                         <li>Aides</li>
+                        <li>Crédits</li>
                         <li>Quitter</li>
                     </ul>
                 </nav>
-            </div>
-        );
-    }
+            </Toggle>
+            <Toggle visible={!open}>{component}</Toggle>
+        </div>
+    );
 };
 
 InterfaceMenuComponent.propTypes = {
