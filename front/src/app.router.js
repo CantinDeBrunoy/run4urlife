2f39586f727bd0063446bc274ab9635bf03b5672
Pierre <payangar@MC-G-LP-C120-15.local>
2022-02-01 17:54:41 +0100

    #26 3d implement

--- front/src/app.router.js
+++ front/src/app.router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import GameComponent from './components/game.component';
 import MenuPage from './pages/menu.page';
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<MenuPage />} />
+            <Route path="/game" element={<GameComponent />} />
         </Routes>
     );
 };
