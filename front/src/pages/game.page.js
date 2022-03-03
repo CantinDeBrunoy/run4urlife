e0726d7d476a835b3376c2483729b6f76e26c4dc
goodfoodtruck <mielatomique@protonmail.com>
2022-03-03 21:05:37 +0100

    init de la branche

--- front/src/pages/game.page.js
+++ front/src/pages/game.page.js
@@ -3,6 +3,7 @@ import BackgroundComponent from '../components/background.component';
 
 import GameComponent from '../components/game.component';
 import { GameProvider } from '../store/game.store';
+import InterfaceInventoryComponent from '../components/interface-inventory.component';
 
 const GamePage = () => {
     return (
@@ -11,6 +12,7 @@ const GamePage = () => {
             <GameProvider>
                 <GameComponent />
             </GameProvider>
+            <InterfaceInventoryComponent />
         </div>
     );
 };
