f64c8de7f24e95d537412bd7abc0b15c925432e4
Cantin Roquier <cantin.roquier@etu.u-pec.fr>
2022-04-07 11:44:38 +0200

    #86 timer

--- front/src/pages/game.page.js
+++ front/src/pages/game.page.js
@@ -4,11 +4,13 @@ import BackgroundComponent from '../components/background.component';
 import GameComponent from '../components/game.component';
 import { GameProvider } from '../store/game.store';
 import InterfaceInventoryComponent from '../components/interface-inventory.component';
+import InterfaceTimerComponent from '../components/interface-timer.component';
 
 const GamePage = () => {
     return (
         <div>
             <BackgroundComponent />
+            <InterfaceTimerComponent />
             <GameProvider>
                 <GameComponent />
                 <InterfaceInventoryComponent />
